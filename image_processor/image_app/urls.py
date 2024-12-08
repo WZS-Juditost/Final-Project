fa049553016573232e00a2b5a81c33ceb8ff29f3
WZS-Juditost <wzswang@ucdavis.edu>
2024-12-08 17:25:14 -0500

    adhere pep8

--- image_processor/image_app/urls.py
+++ image_processor/image_app/urls.py
@@ -2,8 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('convert', views.cartoonize, name='cartoonize'),
-    path('style_transfer/', views.style_transfer_page, name='style_transfer'),
-    path('process_style_transfer', views.style_transfer, name='process_style_transfer'),
+    path(
+        '',
+        views.home,
+        name='home'),
+    path(
+        'convert',
+        views.cartoonize,
+        name='cartoonize'),
+    path(
+        'style_transfer/',
+        views.style_transfer_page,
+        name='style_transfer'),
+    path(
+        'process_style_transfer',
+        views.style_transfer,
+        name='process_style_transfer'),
 ]
